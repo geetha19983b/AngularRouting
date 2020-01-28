807472e0fb79111a103ce097cb414fa010ed2e14
Geethagajalakshmi B <Geethagajalakshmi_B@ad.infosys.com>
2020-01-28 17:53:37 +0530

    initial

---
+++ src/app/products/product-edit/product-edit.component.ts
@@ -0,0 +1,149 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MessageService } from '../../messages/message.service';
+
+import { Product, ProductResolved } from '../product';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent  implements OnInit {
+  pageTitle = 'Product Edit';
+  errorMessage: string;
+
+  //product: Product;
+  private dataIsValid: { [key: string]: boolean } = {};
+
+  ngOnInit(): void {
+   /*  this.route.paramMap.subscribe(
+      params => {
+        const id = +params.get('id');
+        this.getProduct(id);
+      }
+    ); */
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.onProductRetrieved(resolvedData.product);
+    });
+  }
+  get isDirty(): boolean {
+    return JSON.stringify(this.originalProduct) !== JSON.stringify(this.currentProduct);
+  }
+
+  private currentProduct: Product;
+  private originalProduct: Product;
+
+  get product(): Product {
+    return this.currentProduct;
+  }
+  set product(value: Product) {
+    this.currentProduct = value;
+    // Clone the object to retain a copy
+    this.originalProduct = { ...value };
+  }
+  constructor(private productService: ProductService,
+              private messageService: MessageService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.onProductRetrieved(product),
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onProductRetrieved(product: Product): void {
+    this.product = product;
+
+    if (!this.product) {
+      this.pageTitle = 'No product found';
+    } else {
+      if (this.product.id === 0) {
+        this.pageTitle = 'Add Product';
+      } else {
+        this.pageTitle = `Edit Product: ${this.product.productName}`;
+      }
+    }
+  }
+
+  deleteProduct(): void {
+    if (this.product.id === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete(`${this.product.productName} was deleted`);
+    } else {
+      if (confirm(`Really delete the product: ${this.product.productName}?`)) {
+        this.productService.deleteProduct(this.product.id).subscribe({
+          next: () => this.onSaveComplete(`${this.product.productName} was deleted`),
+          error: err => this.errorMessage = err
+        });
+      }
+    }
+  }
+
+  saveProduct(): void {
+    if (true === true) {
+      if (this.product.id === 0) {
+        this.productService.createProduct(this.product).subscribe({
+          next: () => this.onSaveComplete(`The new ${this.product.productName} was saved`),
+          error: err => this.errorMessage = err
+        });
+      } else {
+        this.productService.updateProduct(this.product).subscribe({
+          next: () => this.onSaveComplete(`The updated ${this.product.productName} was saved`),
+          error: err => this.errorMessage = err
+        });
+      }
+    } else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+  }
+  reset(): void {
+    this.dataIsValid = null;
+    this.currentProduct = null;
+    this.originalProduct = null;
+  }
+
+  onSaveComplete(message?: string): void {
+    if (message) {
+      this.messageService.addMessage(message);
+    }
+    this.reset();
+    // Navigate back to the product list
+      // Navigate back to the product list
+      this.router.navigate(['/products']);
+  }
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (this.product.productName &&
+      this.product.productName.length >= 3 &&
+      this.product.productCode) {
+      this.dataIsValid['info'] = true;
+    } else {
+      this.dataIsValid['info'] = false;
+    }
+
+    // 'tags' tab
+    if (this.product.category &&
+      this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true;
+    } else {
+      this.dataIsValid['tags'] = false;
+    }
+  }
+}
